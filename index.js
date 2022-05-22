403927855afc4d9dd8110f1d1d139e00f2c99577
Fábio Palmela <fkadosh@yahoo.com.br>
2022-05-22 16:08:38 -0300

    Criando resolvers para valores não presentes no modelo de dados

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
 const { ApolloServer, gql } = require('apollo-server') 
 
 const typeDefs = gql`
-scalar Date
+    scalar Date
+
+    type Usuario {
+        id: ID
+        nome: String!
+        email: String!
+        idade: Int
+        salario: Float
+        vip: Boolean
+    }
+
+    type Produto {
+        id: ID
+        nome: String!
+        preco: Float!
+        desconto: Float
+        precoComDesconto: Float
+    }
+
     # Pontos de entrada da sua API!
     type Query{
-        ola: String
-        horaAtual: String
-        tipoDate: Date
+        ola: String!
+        horaAtual: Date!
+        tipoDate: Date!
+        usuarioLogado: Usuario
+        produtoEmDestaque: Produto
     }
 ` 
 
 const resolvers = {
+    Usuario: {
+        salario(usuario){
+            return usuario.salario_real
+        }
+    },
+    Produto: {
+        precoComDesconto(produto){
+            var precoDesconto = produto.preco - produto.desconto
+            precoDesconto = parseFloat(precoDesconto.toFixed(2))
+            return precoDesconto
+        }
+    },
     Query: {
         ola(){
             return "Bom dia!"
@@ -22,6 +54,24 @@ const resolvers = {
         tipoDate(){
             var data = new Date()
             return data
+        },
+        usuarioLogado(){
+            return {
+                id: 1,
+                nome: 'Ana',
+                email: 'a@email.com.br',
+                idade: 3,
+                salario_real: 123.44,
+                vip: true
+            }
+        },
+        produtoEmDestaque(){
+            return {
+                id: 1,
+                nome: 'Geladeira',
+                preco: 54.34,
+                desconto: 2.33
+            }
         }
     }
 }
